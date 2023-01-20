d6054e6e67b7643538bffdca366a4344bd8c13a3
Eunice Koo <98850825+euniceseo@users.noreply.github.com>
2023-01-20 00:00:41 -0500

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,22 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+posts = []
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html', posts=posts)
+
+@app.route('/', methods=['POST'])
+def post_comment():
+    text = request.form['text']
+    comment = {'content': text}
+    posts.insert(0, comment)
+    text = ''
+    return render_template('home.html', posts=posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
